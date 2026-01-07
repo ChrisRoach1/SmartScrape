96b711a68e3dfaf335f75eb7d8f7688d74ccf1c9
Christopher Roach <c.roach10@yahoo.com>
2026-01-06 20:33:19 -0500

    SEO stuff

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -12,6 +12,7 @@ import {
 import { Separator } from '@/components/ui/separator';
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { usePathname } from 'next/navigation';
+import Head from 'next/head';
 
 export default function RootLayout({
   children,
@@ -37,32 +38,37 @@ export default function RootLayout({
   }
 
   return (
-    <SidebarProvider
-      style={
-        {
-          '--sidebar-width': '19rem',
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar />
-      <SidebarInset>
-        <header className='flex h-16 shrink-0 items-center gap-2 px-4'>
-          <SidebarTrigger className='-ml-1' />
-          <Separator orientation='vertical' className='mr-2 data-[orientation=vertical]:h-4' />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className='hidden md:block'>
-                <BreadcrumbLink href='/dashboard'>SmartScrape</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className='hidden md:block' />
-              <BreadcrumbItem>
-                <BreadcrumbPage>{breadcrumb}</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </header>
-        <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>{children}</div>
-      </SidebarInset>
-    </SidebarProvider>
+    <>
+      <Head>
+        <meta name='robots' content='noindex, nofollow' />
+      </Head>
+      <SidebarProvider
+        style={
+          {
+            '--sidebar-width': '19rem',
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar />
+        <SidebarInset>
+          <header className='flex h-16 shrink-0 items-center gap-2 px-4'>
+            <SidebarTrigger className='-ml-1' />
+            <Separator orientation='vertical' className='mr-2 data-[orientation=vertical]:h-4' />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className='hidden md:block'>
+                  <BreadcrumbLink href='/dashboard'>SmartScrape</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className='hidden md:block' />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>{breadcrumb}</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </header>
+          <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>{children}</div>
+        </SidebarInset>
+      </SidebarProvider>
+    </>
   );
 }
